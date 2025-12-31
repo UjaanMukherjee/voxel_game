bb7bdacf981f0ced6e0d022dbeb4be77525087b4
Ujaan Mukherjee <ujaan@LAPTOP-8GO6RITM.localdomain>
2025-12-31 17:29:25 +0000

    Added 2d plane

--- lib/main.dart
+++ lib/main.dart
@@ -2,8 +2,16 @@ import 'dart:ui';
 
 import 'package:flutter/material.dart';
 import 'package:flutter/scheduler.dart';
+import 'package:flutter/services.dart';
+import 'package:vector_math/vector_math_64.dart' hide Colors;
 
-void main() {
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  await SystemChrome.setPreferredOrientations([
+    DeviceOrientation.landscapeLeft,
+    DeviceOrientation.landscapeRight,
+  ]);
+  await SystemChrome.setEnabledSystemUIMode(SystemUiMode.immersiveSticky);
   runApp(const MyGame());
 }
 
@@ -32,40 +40,72 @@ class Screen extends StatefulWidget {
 class _ScreenState extends State<Screen> {
   late Ticker _ticker;
   Duration _lastElapsed = Duration.zero;
+  late final ValueNotifier<Vector2> playerPosition;
   @override
   void initState() {
+    playerPosition = ValueNotifier(Vector2(100, 100));
     _ticker = Ticker((elapsed) {
       // calculate tick
-      final dt = (elapsed - _lastElapsed) / 1000000.0;
+
+      final dt = (elapsed - _lastElapsed).inMicroseconds / 1000000.0;
       _lastElapsed = elapsed;
       update();
       render();
     });
+
     _ticker.start();
 
     super.initState();
   }
 
+  @override
+  void dispose() {
+    playerPosition.dispose();
+
+    super.dispose();
+  }
+
   void update() {}
   void render() {}
 
+  void updatePlayerPosition(Vector2 directionVector) {
+    directionVector = directionVector.normalized() * 5;
+    playerPosition.value = Vector2(
+      playerPosition.value.x + directionVector.x,
+      playerPosition.value.y + directionVector.y,
+    );
+  }
+
   @override
   Widget build(BuildContext context) {
-    return Container(
-      height: widget.height,
-      width: widget.width,
-      color: Colors.grey,
-      child: CustomPaint(painter: PlayerPainter()),
+    return GestureDetector(
+      onTapUp: (details) => updatePlayerPosition(
+        Vector2(
+          details.globalPosition.dx - playerPosition.value.x,
+          details.globalPosition.dy - playerPosition.value.y,
+        ),
+      ),
+
+      child: Container(
+        height: widget.height,
+        width: widget.width,
+        color: Colors.grey,
+        child: CustomPaint(painter: PlayerPainter(repaint: playerPosition)),
+      ),
     );
   }
 }
 
 class PlayerPainter extends CustomPainter {
+  final ValueNotifier<Vector2> repaint;
+
+  PlayerPainter({required this.repaint}) : super(repaint: repaint);
+
   @override
   void paint(Canvas canvas, Size size) {
     final size = 10.0;
     final paint = Paint()..color = Colors.yellow;
-    final rect = Rect.fromLTWH(100, 100, size, size);
+    final rect = Rect.fromLTWH(repaint.value.x, repaint.value.y, size, size);
     canvas.drawRect(rect, paint);
   }
 
